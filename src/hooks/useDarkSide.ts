0e50c3947849e67d6c7e407fe7bbcf826ba0c6db
nguyenhoangloc2208 <nguyenhoangloc2208@gmail.com>
2024-05-12 19:38:22 +0700

    reconfiguration useDarkSide hook

--- src/hooks/useDarkSide.ts
+++ src/hooks/useDarkSide.ts
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
 
 export default function useDarkSide(): [string, (theme: string) => void, boolean] {
-  const [theme, setTheme] = useState<string>(() => localStorage.theme || 'dark');
+  const [theme, setTheme] = useState<string>(() => localStorage.theme || 'light');
   const [loading, setLoading] = useState<boolean>(true);
+  const [initialized, setInitialized] = useState<boolean>(false);
   const colorTheme: string = theme === 'dark' ? 'light' : 'dark';
+
+  useEffect(() => {
+    const root: HTMLElement = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    // save theme to local storage
+    localStorage.setItem('theme', theme);
+
+    // Khi hiệu ứng phụ này kết thúc, đánh dấu đã khởi tạo hook.
+    setInitialized(true);
+  }, [theme, colorTheme]);
 
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme');
@@ -12,14 +25,7 @@ export default function useDarkSide(): [string, (theme: string) => void, boolean
     } else {
       setLoading(false);
     }
+  }, []);
 
-    const root: HTMLElement = window.document.documentElement;
-    root.classList.remove(colorTheme);
-    root.classList.add(theme);
-
-    // save theme to local storage
-    localStorage.setItem('theme', theme);
-  }, [theme, colorTheme]);
-
-  return [colorTheme, setTheme, loading];
+  return [colorTheme, setTheme, loading && !initialized];
 }
